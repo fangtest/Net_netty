c76fa0c3c086adee0ce75f105b3b62dbf599cd8c
fangtest <fangxu@comisys.net>
2018-02-24 18:47:36 +0800

    [feature] init

---
+++ src/com/gbn48/net/netty/TimeClient.java
@@ -0,0 +1,42 @@
+package com.gbn48.net.netty;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+/**
+ * @author fangtest on 2018/2/24.
+ */
+public class TimeClient {
+
+    public static void main(String[] args) throws Exception {
+        int port = 8080;
+        new TimeClient().connect(port, "127.0.0.1");
+    }
+
+    private void connect(int port, String host) throws Exception{
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(group).channel(NioSocketChannel.class)
+                     .option(ChannelOption.TCP_NODELAY, true)
+                     .handler(new ChannelInitializer<SocketChannel>() {
+                         @Override
+                         protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new TimeClientHandler());
+                         }
+                     });
+            ChannelFuture future = bootstrap.connect(host, port).sync();
+            future.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully();
+        }
+
+
+    }
+}
---
+++ src/com/gbn48/net/netty/TimeServer.java
@@ -0,0 +1,50 @@
+package com.gbn48.net.netty;
+
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * @author fangtest on 2018/2/24.
+ */
+public class TimeServer {
+
+
+    public static void main(String[] args) throws Exception {
+        int port = 8080;
+        new TimeServer().bind(port);
+    }
+
+    private void bind(int port) throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class)
+                     .option(ChannelOption.SO_BACKLOG, 1024)
+                     .childHandler(new ChildChannelHandler());
+
+            ChannelFuture future = bootstrap.bind(port).sync();
+            future.channel().closeFuture().sync();
+
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    private class ChildChannelHandler extends ChannelInitializer<SocketChannel> {
+
+        @Override
+        protected void initChannel(SocketChannel ch) throws Exception {
+            ch.pipeline().addLast(new TimeServerHandler());
+        }
+    }
+}
